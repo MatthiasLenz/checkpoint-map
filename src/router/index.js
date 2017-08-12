84278c33402f0816f3f56e59a54b5635c2183a77
Matthias Lenz <mttlnz@gmail.com>
2017-08-12 20:53:01 +0200

    geocode search

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import CheckpointMap from '@/components/CheckpointMap'
 
 Vue.use(Router)
@@ -9,11 +8,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/map',
       name: 'Map',
       component: CheckpointMap
     }
